b60d9fcdd8127aa10f9936963e3289d163af0699
star-liu888 <starliu1224@gmail.com>
2026-09-08 09:14:06 +0800

    Add files via upload

---
+++ data/__init__.py
@@ -0,0 +1,19 @@
+from .data import (
+    BatterySample,
+    Case1Dataset,
+    Normalizer,
+    apply_saved_split,
+    discover_case1_files,
+    load_case1_samples,
+    split_case1_samples,
+)
+
+__all__ = [
+    "BatterySample",
+    "Case1Dataset",
+    "Normalizer",
+    "apply_saved_split",
+    "discover_case1_files",
+    "load_case1_samples",
+    "split_case1_samples",
+]
---
+++ data/public_datasets.md
@@ -0,0 +1,4 @@
+# Public datasets
+
+- [Kaggle dataset](https://www.kaggle.com/dongzhenlyu/datasets)
+- [Zenodo dataset](https://doi.org/10.5281/zenodo.6379165)
